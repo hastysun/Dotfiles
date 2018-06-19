f1433d74a5cb40d82413369f98868a51240d6fc1
Gavin Weiss <hasty459@gmail.com>
2018-06-19 15:49:58 -0400

    Testing stuff

---
+++ dwm/themes.h
@@ -0,0 +1,40 @@
+// Gavin Weiss
+// June 2018
+
+// themes.h
+
+
+// Functions
+/*
+static const Theme theme[] = {
+
+	{ "arizona",	arizona },
+	{ "mint", 		mint },
+
+};
+*/
+
+
+
+
+#define Arizona
+
+#ifdef Mint
+static const char normbordercolor[] = "#949093";
+static const char normbgcolor[]     = "#151b1e";
+static const char normfgcolor[]     = "#d4ced2";
+static const char selbordercolor[]  = "#d4ced2";
+static const char selbgcolor[]      = "#619668";
+static const char selfgcolor[]      = "#d4ced2";
+#endif
+
+
+
+#ifdef Arizona
+static const char normbordercolor[] = "#949093";
+static const char normbgcolor[]     = "#151b1e";
+static const char normfgcolor[]     = "#d4ced2";
+static const char selbordercolor[]  = "#d4ced2";
+static const char selbgcolor[]      = "#689990";
+static const char selfgcolor[]      = "#d4ced2";
+#endif
